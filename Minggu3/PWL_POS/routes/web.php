8ee3b940093871bccae46af8827cb88d2cda29cc
maisyadhia <maisyaazzahra05@gmail.com>
2025-03-03 22:43:18 +0700

    Implementasi Query Builder

--- Minggu3/PWL_POS/routes/web.php
+++ Minggu3/PWL_POS/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
-Route::get('/level', [LevelController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);

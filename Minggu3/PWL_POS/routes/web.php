0d515c5707782d24c4154737b4bcd3515477ed59
maisyadhia <maisyaazzahra05@gmail.com>
2025-03-03 22:13:06 +0700

    Implementasi DB Facade

--- Minggu3/PWL_POS/routes/web.php
+++ Minggu3/PWL_POS/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/level', [LevelController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });

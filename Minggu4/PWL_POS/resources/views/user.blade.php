1ab68245fe2d11be9f2bf90a66d2ed701892c681
maisyadhia <maisyaazzahra05@gmail.com>
2025-03-07 18:38:47 +0700

    Create, Read, Update, Delete (CRUD)

--- Minggu4/PWL_POS/resources/views/user.blade.php
+++ Minggu4/PWL_POS/resources/views/user.blade.php
@@ -7,27 +7,30 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="/user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            {{-- <th>Jumlah Pengguna</th> --}}
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            {{-- <td>Jumlah Pengguna</td> --}}
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
         </tr>
-        {{-- @foreach ($data as $user) --}}
+        @foreach ($data as $d)
             <tr>
                 {{-- <td>{{ $jumlahUser }}</td> --}}
                 {{-- <td>{{ $user->user_id }}</td>
                 <td>{{ $user->username }}</td>
                 <td>{{ $user->nama }}</td>
                 <td>{{ $user->level_id }}</td> --}}
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
             </tr>
-        {{-- @endforeach --}}
+        @endforeach
     </table>
 </body>
 </html>

6882b995f85c7f5efaf68b2fb5286f43668aff7c
maisyadhia <maisyaazzahra05@gmail.com>
2025-03-24 10:40:13 +0700

    praktikum-1 tambah view layouts

--- Minggu5/PWL_POS/database/seeders/DatabaseSeeder.php
+++ Minggu5/PWL_POS/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            LevelSeeder::class,
+            UserSeeder::class,
+            KategoriSeeder::class,
+            BarangSeeder::class,
+            StokSeeder::class,
+            PenjualanSeeder::class,
+            PenjualanDetailSeeder::class,
         ]);
     }
 }

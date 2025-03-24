e94192a56954c679957416587ead151cb35e3694
maisyadhia <maisyaazzahra05@gmail.com>
2025-03-24 20:48:06 +0700

    Praktikum 4- filtering datatable

--- Minggu5/PWL_POS/resources/views/user/index.blade.php
+++ Minggu5/PWL_POS/resources/views/user/index.blade.php
@@ -18,7 +18,24 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-        
+    
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select class="form-control" id="level_id" name="level_id" required>
+                                <option value="">- Semua -</option>
+                                @foreach($level as $item)
+                                    <option value="{{$item->level_id }}"> {{$item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                         </div>
+                    </div>
+                </div>
+            </div>
+
             <!-- Tabel untuk menampilkan data user -->
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
@@ -45,8 +62,13 @@
                 // serverSide: true, jika ingin menggunakan server side processing 
                 serverSide: true,
                 ajax: {
-                    "url": "{{ url('user/list') }}", "dataType": "json",
-                    "type": "POST"
+                    "url": "{{ url('user/list') }}",
+                    "dataType": "json",
+                    "type": "POST",
+                    "data" : function (d){
+                        d.level_id = $('#level_id').val();
+                    }
+
                 },
                 columns: [
                 {
@@ -67,6 +89,9 @@
                     className: "", orderable: false, searchable: false
                 }
                 ]
+            });
+            $('#level_id').on( 'change', function() {
+                dataUser.ajax.reload();
             });
         });
     </script> 

1d7d0a7f2cf2b913a8ac888bab79b33a370d2ed6
maisyadhia <maisyaazzahra05@gmail.com>
2025-03-28 09:32:40 +0700

    jquery-validation;SweetAlert2

--- Minggu6/PWL_POS/resources/views/layouts/template.blade.php
+++ Minggu6/PWL_POS/resources/views/layouts/template.blade.php
@@ -76,6 +76,14 @@
 <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.colvis.min.js') }}"></script>
+
+<!-- jquery-validation -->
+<script src="{{ asset( 'plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('plugins/jquery-validation/additional-methods.min.js') }}"></script>
+
+<!-- SweetAlert2 -->
+<script src="{{ asset( 'plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+
 <!-- AdminLTE App -->
 <script src="{{ asset('adminlte/plugins/dist/js/adminlte.min.js') }}"></script>
 <script>

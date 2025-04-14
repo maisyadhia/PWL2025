d6e9c9670f3ec88d156d65b29943e0e8e0ede750
maisyadhia <maisyaazzahra05@gmail.com>
2025-04-14 18:05:48 +0700

    Prak2

--- Minggu8/PWL_POS/routes/web.php
+++ Minggu8/PWL_POS/routes/web.php
@@ -148,6 +148,7 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}',[BarangController::class,'destroy']);
         Route::get('/import', [BarangController::class, 'import']);
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
+        Route::get('/export_excel', [BarangController::class, 'export_excel']);
     });
 
     Route::middleware(['authorize:ADM,MNG,STF'])->prefix('supplier')->group(function () {

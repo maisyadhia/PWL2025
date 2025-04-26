79a22486f6bfd2b01f28c43cb7564567402804fd
maisyadhia <maisyaazzahra05@gmail.com>
2025-04-26 13:04:32 +0700

    praktikum 4

--- Minggu10/PWL_POS/routes/api.php
+++ Minggu10/PWL_POS/routes/api.php
@@ -4,10 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LevelController;
 
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
 Route::post ('/logout', App\Http\Controllers\Api\LogoutController:: Class) ->name('logout');
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();

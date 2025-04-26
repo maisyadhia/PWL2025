2d8516e47d52fbedd68d6e50c4c25d4db9750d23
maisyadhia <maisyaazzahra05@gmail.com>
2025-04-26 07:07:02 +0700

    Praktikum 1

--- Minggu10/PWL_POS/app/Models/UserModel.php
+++ Minggu10/PWL_POS/app/Models/UserModel.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\LevelModel;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     use HasFactory;
 
     protected $table = 'm_user';
